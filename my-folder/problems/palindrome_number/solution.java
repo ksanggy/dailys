9ad1b56de05b86c96276052ec0b8473adb207fec
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2022-09-25 04:44:35 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- my-folder/problems/palindrome_number/solution.java
+++ my-folder/problems/palindrome_number/solution.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String tempNumString = String.valueOf(x);
-
-        //reverse
-        StringBuffer sb = new StringBuffer(tempNumString);
-        String reversedStrNum = sb.reverse().toString();
-        return tempNumString.compareTo(reversedStrNum) == 0;
+        int originalNum = x;
+        int reversedNum = 0;
+        if(originalNum < 0 || (x%10 == 0 && x != 0)) return false;
+        while(originalNum != 0){
+            int remainder = originalNum % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            originalNum /= 10; 
+        }
+        if(reversedNum == x){
+            return true;
+        }
+        return false;
     }
 }
