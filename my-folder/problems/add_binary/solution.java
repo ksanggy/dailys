2889535585bc8edb2194f9208f58e692311224ab
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2022-10-03 06:02:04 +0000

    Sync LeetCode submission - Add Binary (java)

---
+++ my-folder/problems/add_binary/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addBinary(String a, String b) {
+    int carrier = 0;
+    int maxLength = Integer.max(a.length(), b.length());
+    String result = "";
+    for(int i = 0; i < maxLength; i++){
+        int value1 = i < a.length() ? a.charAt(a.length()-1 -i) - '0' : 0;
+        int value2 = i < b.length() ? b.charAt(b.length()-1 -i) - '0' : 0;
+        int tmp = value1 + value2 + carrier;
+        carrier = tmp /2;
+        result = tmp % 2 + result;
+    }
+    return (carrier == 0) ? result : "1" + result;
+    }
+}
