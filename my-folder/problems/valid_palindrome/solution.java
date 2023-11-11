89408ad5fd96a17674e5d13a4b1a0a9b6466498d
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2023-11-11 03:39:36 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- my-folder/problems/valid_palindrome/solution.java
+++ my-folder/problems/valid_palindrome/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if(s==null)  return false;
-        s = s.toLowerCase();
         int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            while(left<right && !((s.charAt(left)>='a' && s.charAt(left)<='z') || (s.charAt(left)>='0'&&s.charAt(left)<='9'))){
+        int right = s.length() - 1;
+        while(left < right) {
+            // increment left pointer to skip characters that are non alphanumeric
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
-            while(left<right && !((s.charAt(right)>='a' && s.charAt(right)<='z') || (s.charAt(right)>='0'&&s.charAt(right)<='9'))){
+            // increment right pointer to skip characters that are non alphanumeric
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
-            if(s.charAt(left) != s.charAt(right)){
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
                 return false;
-            }
             left++;
             right--;
         }
         return true;
-        
     }
 }
