b9fb0932fb152b23c2d71c7d8ba53af4a3fa792c
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2023-11-22 02:12:29 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- my-folder/problems/merge_two_sorted_lists/solution.java
+++ my-folder/problems/merge_two_sorted_lists/solution.java
@@ -12,27 +12,26 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null) return list2;
         if(list2 == null) return list1;
-        
-        ListNode headNode = new ListNode(0);
-        ListNode headNext = headNode;
-
-        while(list1 != null && list2 != null){
-            if(list1.val > list2.val){
-                headNext.next = list2;
+        // 1. set up init variable 'mergedList'
+        ListNode mergedList = new ListNode(0);
+        ListNode head = mergedList;
+        // 2. loop through to compare each nodes values
+        while(list1 != null && list2 != null) {
+            if(list1.val > list2.val) {
+                head.next = list2;
                 list2 = list2.next;
-            }else {
-                headNext.next = list1;
+            } else {
+                head.next = list1;
                 list1 = list1.next;
             }
-            headNext = headNext.next;
+            head = head.next;
         }
 
-        if(list1 == null){
-            headNext.next = list2;
-        }
-        if(list2 == null){
-            headNext.next = list1;
-        }
-        return headNode.next;
+        // 3. check which list reached the end first
+        if(list1 == null)
+            head.next = list2;
+        else
+            head.next = list1;
+        return mergedList.next; // since we have a dummy of val 0 at head
     }
 }
