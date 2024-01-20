4952cb9f2dcefceae98c66e27555c428761a9c3e
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-01-20 22:26:55 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- my-folder/problems/valid_parentheses/solution.java
+++ my-folder/problems/valid_parentheses/solution.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stackedChars = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            char item;
-            char theChar = s.charAt(i);
-
-            if(theChar == '(' || theChar == '{' || theChar == '['){
-                stackedChars.push(theChar);
-            }else {
-                try{
-                    item = stackedChars.pop();
-                } catch(Exception e){
+        // base case
+        if(s.length() % 2 != 0)
+            return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> bracketLookup = new HashMap<>();
+        // setup lookup table with key being the closing parenthesis
+        bracketLookup.put(')', '(');
+        bracketLookup.put('}', '{');
+        bracketLookup.put(']', '[');
+        for (char c : s.toCharArray()) {
+            if(bracketLookup.containsKey(c)) {
+                // you are adding to stack only opening parenthesis
+                //  if the current character is in bracketLookup
+                // (which key holds the closing) then stack.peek() need to == the value of lookup
+                if(!stack.isEmpty() && bracketLookup.get(c).equals(stack.peek()))
+                    stack.pop(); // remove from stack as we have found the pair
+                else
                     return false;
-                }
-
-                if((theChar ==')' && item != '(') || (theChar == '}' && item != '{') || (theChar == ']' && item != '[')){
-                    return false;
-                }
-            }
+            } else
+                stack.push(c);
         }
-        return stackedChars.isEmpty();
+        return stack.isEmpty(); // if valid, stack needs to be empty
     }
 }
