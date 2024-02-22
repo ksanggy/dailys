a8b346ffbc0a3e150ba02d13a7d0be279ace297d
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-02-22 00:45:19 +0000

    Sync LeetCode submission - 3Sum (java)

--- my-folder/problems/3sum/solution.java
+++ my-folder/problems/3sum/solution.java
@@ -2,10 +2,10 @@ class Solution {
     public static List<List<Integer>> threeSum( int[] nums) {
         List<List<Integer>> triplets = new ArrayList<>(); // the list of triplets that will be returned as a result
         // sort the array first
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            // skip repeating values
-            if(i > 0 && nums[i] == nums[i-1])
+        Arrays.sort(nums); // this is O(nlogn)
+        // loop through every element and search for pair that equates to the current element of below loop
+        for(int i = 0; i < nums.length; i++) {
+            if(i != 0 && nums[i] == nums[i-1])
                 continue;
             searchPair(nums, -nums[i], i + 1, triplets);
         }
@@ -13,21 +13,24 @@ class Solution {
     }
 
     private static void searchPair( int[] nums, int target, int left, List<List<Integer>> triplets ) {
-        List<Integer> triplet = new ArrayList<>();
         int right = nums.length - 1;
         while(left < right) {
-            int pairSum = nums[left] + nums[right];
-            if(pairSum == target) {
+            int currSum = nums[left] + nums[right];
+            if(currSum == target) {
                 triplets.add(Arrays.asList(-target, nums[left], nums[right]));
                 left++;
                 right--;
-                while(left < right && nums[left] == nums[left-1]) left++;
-                while(left < right && nums[right] == nums[right+1]) right--;
+                while(left < right && nums[left] == nums[left - 1])
+                    left++;
+                while(left < right && nums[right] == nums[right + 1])
+                    right--;
             }
-            else if(pairSum < target)
+            else if(currSum < target) {
                 left++;
-            else
+            }
+            else {
                 right--;
+            }
         }
     }
 }
