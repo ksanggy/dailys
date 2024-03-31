ee3523e4bec943864d588b7449311fb71941d93e
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-03-31 13:31:26 +0000

    Sync LeetCode submission - LRU Cache (java)

--- my-folder/problems/lru_cache/solution.java
+++ my-folder/problems/lru_cache/solution.java
@@ -1,26 +1,75 @@
-class LRUCache extends LinkedHashMap<Integer, Integer> {
+class LRUCache {
 
-    private final int capacity;
+    private Map<Integer, Node> cache;
+    private int capacity;
+    private Node left;
+    private Node right;
 
     public LRUCache(int capacity) {
-        super(capacity, 1.0f, true);
         this.capacity = capacity;
+        cache = new HashMap<>();
+
+        // left = LRU, right = most recent
+        this.left = new Node(0,0);
+        this.right = new Node(0, 0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
     }
 
-   
-    @Override
-    protected boolean removeEldestEntry( Map.Entry<Integer, Integer> eldest ) {
-        return super.size() > capacity;
-    }
-
-    public int get( int key) {
-        return super.getOrDefault(key, -1);
+    public int get(int key) {
+        if(cache.containsKey(key)){
+            remove(cache.get(key));
+            insert(cache.get(key));
+            return cache.get(key).val;
+        }
+        return -1;
     }
 
     public void put(int key, int value) {
-        super.put(key,value);
+        if(cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        cache.put(key, new Node(key, value));
+        insert(cache.get(key));
+        if(cache.size() > capacity) {
+            // remove from the list and delete the LRU from the hashmap
+            Node lru = this.left.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void insert(Node node) {
+        Node prev = this.right.prev;
+        Node next = this.right;
+
+        prev.next = node;
+        next.prev = node;
+
+        node.next = next;
+        node.prev = prev;
+    }
+
+    private class Node {
+        int key;
+        int val;
+        Node next;
+        Node prev;
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
