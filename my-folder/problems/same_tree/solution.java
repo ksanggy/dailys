03be9092c0fc429a7acdf4638962a7cd88787d3a
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-04-12 02:11:09 +0000

    Sync LeetCode submission - Same Tree (java)

--- my-folder/problems/same_tree/solution.java
+++ my-folder/problems/same_tree/solution.java
@@ -14,17 +14,10 @@
  * }
  */
 class Solution {
-    public static boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p, q);
-    }
-
-    private static boolean dfs( TreeNode p, TreeNode q ) {
-        if(p == null && q == null)
-            return true;
-
-        if(p == null || q == null || p.val != q.val)
-            return false;
-
-        return dfs(p.left, q.left) && dfs(p.right, q.right);
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
