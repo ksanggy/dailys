8985a44b34fbf5c00f73c429a4b8e197102f362f
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-05-16 16:52:26 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- my-folder/problems/binary_tree_level_order_traversal/solution.java
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -16,28 +16,19 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        // base case
-        if(root == null)
-            return result;
-
-        // instantiate queue to hold current level node
+        if (root == null) return result;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        // work through all nodes until queue is empty
-        while(!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> levelList = new ArrayList<>(levelSize);
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode current = queue.poll();
-                levelList.add(current.val);
-                // if left node is not empty add to queue for next procedure
-                if(current.left != null)
-                    queue.offer(current.left);
-                // if right node is not empty add to queue for next procedure
-                if(current.right != null)
-                    queue.offer(current.right);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
             }
-            result.add(levelList);
+            result.add(level);
         }
         return result;
     }
