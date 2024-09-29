085ac6fb383c10b1cc8e3a3a81fe351aa633dcc2
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-09-29 23:52:04 +0000

    Sync LeetCode submission - Reverse Vowels of a String (java)

--- my-folder/problems/reverse_vowels_of_a_string/solution.java
+++ my-folder/problems/reverse_vowels_of_a_string/solution.java
@@ -1,38 +1,24 @@
 class Solution {
     public String reverseVowels(String s) {
-        Map<Character, Integer> vowels = Map.ofEntries(
-            Map.entry('a', 1),
-            Map.entry('e', 1),
-            Map.entry('i', 1),
-            Map.entry('o', 1),
-            Map.entry('u', 1),
-            Map.entry('A', 1),
-            Map.entry('E', 1),
-            Map.entry('I', 1),
-            Map.entry('O', 1),
-            Map.entry('U', 1)
-        );
-
+        final String vowels = "aeiouAEIOU";
         int left = 0;
         int right = s.length() - 1;
         char[] chars = s.toCharArray();
-        
 
         while(left < right) {
-            char leftChar = chars[left];
-            char rightChar = chars[right];
 
-            if(!vowels.containsKey(leftChar)) {
+            while(left < right && vowels.indexOf(chars[left]) == -1) {
                 left++;
-                continue;
-            }
-            if(!vowels.containsKey(rightChar)) {
-                right--;
-                continue;
             }
 
-            chars[left] = rightChar;
-            chars[right] = leftChar;
+            while(left < right && vowels.indexOf(chars[right]) == -1) {
+                right--;
+            }
+
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
             left++;
             right--;
         }
