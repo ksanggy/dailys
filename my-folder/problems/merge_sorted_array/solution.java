bc3012c1a5734adc552c87d491b18a1f345149cb
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2024-10-11 03:23:55 +0000

    Sync LeetCode submission - Merge Sorted Array (java)

--- my-folder/problems/merge_sorted_array/solution.java
+++ my-folder/problems/merge_sorted_array/solution.java
@@ -1,15 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1; // length of nums1 with actual values (the none 0s)
-        int j = n - 1; // length of nums2
-        int k = nums1.length - 1; // this the actual length of the merged array m + n
+        int i = m - 1;
+        int j = n - 1;
+        int k = nums1.length - 1; // the total length of m + n array length
 
-        // The plan is to merge and place values from the end since the two input arrays are sorted
-        while(j >= 0) {
-            // if 'nums1' value is greater, then place the nums1 value in nums1[k--] index then decrement by 1
+        // loop through until nums2 is looped
+        while (j >= 0) {
             if(i >= 0 && nums1[i] > nums2[j])
                 nums1[k--] = nums1[i--];
-            // else, then place the nums2 value in nums1[k--] index then decrement by 1
             else
                 nums1[k--] = nums2[j--];
         }
