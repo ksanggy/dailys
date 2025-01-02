0df9b0637577ec3d7376dc997f53b1917e258c90
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2025-01-02 17:49:48 +0000

    Sync LeetCode submission - Merge Strings Alternately (java)

--- my-folder/problems/merge_strings_alternately/solution.java
+++ my-folder/problems/merge_strings_alternately/solution.java
@@ -1,23 +1,22 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int pointer1 = 0;
-        int pointer2 = 0;
-        char[] word1Chars = word1.toCharArray();
-        char[] word2Chars = word2.toCharArray();
-        StringBuilder mergedSB = new StringBuilder();
-        
-        while(pointer1 < word1Chars.length && pointer2 < word2Chars.length) {
-            mergedSB.append(word1Chars[pointer1]);
-            mergedSB.append(word2Chars[pointer2]);
-            pointer1++;
-            pointer2++;
-        }
+        int word1Length = word1.length() - 1;
+        int word2Length = word2.length() - 1;
+        int lengthToLoop = Math.min(word1.length(), word2.length());
+        StringBuilder sb = new StringBuilder();
 
-        if(pointer1 < word1Chars.length)
-            mergedSB.append(word1.substring(pointer1));
-        if(pointer2 < word2Chars.length)
-            mergedSB.append(word2.substring(pointer2));
-        
-        return mergedSB.toString();
+        for(int i = 0; i <= lengthToLoop; i++) {
+            if(i > word1Length) {
+                sb.append(word2.substring(i, word2Length + 1));
+                break;
+            }    
+            if(i > word2Length) {
+                sb.append(word1.substring(i, word1Length + 1));
+                break;
+            }
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+        return sb.toString();
     }
 }
