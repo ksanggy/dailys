4255d30f1b2bee4cedda0cb42895999222d80c4f
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2025-01-08 18:52:03 +0000

    Sync LeetCode submission - Container With Most Water (java)

--- my-folder/problems/container_with_most_water/solution.java
+++ my-folder/problems/container_with_most_water/solution.java
@@ -1,22 +1,20 @@
 class Solution {
+    // init intuition :
+    // if we were to just solve by the flow of what is given, then TC = quadratic
+    // two pointer to compare and calculate area to find max container will be more efficient.
+    // it boils down to how or when to move either of the two pointers.
     public int maxArea(int[] height) {
-        // 0. base case
-        if(height.length == 0)
-            return 0;
-        // 1. setup pointers and variable maxArea to hold the maximum area combination
         int left = 0;
-        int right = height.length - 1;
-        int maxArea = 0;
-        // 2. loop through while let < right to find the maximum amount of water container can hold
+        int right = height.length - 1; // start from the other end to maximize efficiency as index plays a role in calculating area.
+        int maxArea = 0; // max area to return
         while(left < right) {
-            int minHeight = Math.min(height[left], height[right]);
-            int xAxisDistance = right - left;
-            maxArea = Math.max(maxArea, minHeight * xAxisDistance);
+            int currHeight = Math.min(height[left], height[right]);
+            int area = Math.abs((left - right) * currHeight);
+            maxArea = Math.max(maxArea, area);
 
-            // if left pointer element is less than or equal to right element then increment left
-            if(height[left] <= height[right])
+            // we move the pointer that is of a smaller value
+            if(height[left] < height[right])
                 left++;
-            // else increment right
             else
                 right--;
         }
