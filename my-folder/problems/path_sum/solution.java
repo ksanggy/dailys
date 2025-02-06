68b953ccb033d0f075241aafdad0c4b7a9659f6b
Sangho Kim <66589936+ksanggy@users.noreply.github.com>
2025-02-06 19:18:20 +0000

    Sync LeetCode submission - Path Sum (java)

--- my-folder/problems/path_sum/solution.java
+++ my-folder/problems/path_sum/solution.java
@@ -15,13 +15,12 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        // base case
-        if (root == null)
+        if(root == null)
             return false;
 
         if(root.left == null && root.right == null && root.val == targetSum)
             return true;
-
+        
         return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
